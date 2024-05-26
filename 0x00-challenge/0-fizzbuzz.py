924353354b5bf7840ec41bc5a043662f1946526e
EliezerSunny <adetunjieliazer@gmail.com>
2024-05-26 08:19:26 +0000

    modified

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ FizzBuzz
-    Change of logic if (i % 3) == 0 and (i % 5) == 0:
 """
 import sys
 
@@ -18,10 +17,10 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
+        if (i % 3) == 0:
             tmp_result.append("Fizz")
+        elif (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -4,8 +4,6 @@
     
     The size of the square must be the first argument 
     of the program.
-
-    change in size = parseInt(process.argv[2], 16)
 */
 
 
@@ -16,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10)
+size = parseInt(process.argv[2], 16)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
