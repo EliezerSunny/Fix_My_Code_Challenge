50ccec1a89c71e2feb835587e1ea569e13c46025
EliezerSunny <adetunjieliazer@gmail.com>
2024-05-26 08:30:49 +0000

    modified

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,19 +6,24 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 16)
+const size = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+if (isNaN(size)) {
+    process.stderr.write("Invalid argument\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
+        line += '#';
     }
-    process.stdout.write("\n");
+    console.log(line);
 }
